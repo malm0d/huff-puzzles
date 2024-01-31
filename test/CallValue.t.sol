0b287563b7d3f0561cfcdf1bc297aab3727306e9
Malcolm Tan <malcolmtan@Malcolms-MacBook-Air.local>
2024-01-31 23:10:47 +0800

    Update CallValue

--- test/CallValue.t.sol
+++ test/CallValue.t.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.19;
 
-import "forge-std/Test.sol";
+import {Test, console2} from "forge-std/Test.sol";
 import {HuffConfig} from "foundry-huff/HuffConfig.sol";
 import {HuffDeployer} from "foundry-huff/HuffDeployer.sol";
 
 interface CallValue {}
 
+//forge test -vvvv --mc CallValueTest
 contract CallValueTest is Test {
     CallValue public callValue;
 
@@ -17,6 +18,7 @@ contract CallValueTest is Test {
     function testCallValue(uint256 value) public {
         vm.deal(address(this), value);
         (bool success, bytes memory retdata) = address(callValue).call{value: value}("");
+        console2.logBytes(retdata);
         require(success, "call failed");
         assertEq(abi.decode(retdata, (uint256)), value, "Wrong retdata");
     }
